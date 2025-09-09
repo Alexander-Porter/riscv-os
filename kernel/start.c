4e18bff1682a6e3ca9f004dbdc13225d18aa6e6f
Alexander-Porter <pingri.lin@gmail.com>
2025-09-09 16:45:04 +0800

    bss测试。结果：没发现不清空的问题

--- kernel/start.c
+++ kernel/start.c
@@ -1,6 +1,8 @@
 #include "global_func.h"
 void main();
 
+int bss_test; // 用于测试 .bss 段是否被正确清零
+float bss_test_float; // 测试 .bss 段的浮点数变量
 
 // entry.S needs one stack per CPU.
 __attribute__ ((aligned (16))) char stack0[4096];
@@ -11,5 +13,9 @@ start()
 {
 uart_putc('T'); // 输出字符S表示启动
 uart_puts("Hello, world!\n");
+if (bss_test != 0 || bss_test_float != 0.0f) {
+    uart_puts("Error: .bss segment not cleared!\n");
+} else {
+    uart_puts(".bss segment cleared successfully.\n");
 }
-
+}
