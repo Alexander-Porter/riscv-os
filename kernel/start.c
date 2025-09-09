00352c0e4b124a1f75d62a6e2f0ebbc05abed37a
Alexander-Porter <pingri.lin@gmail.com>
2025-09-09 16:02:25 +0800

    成功启动系统

---
+++ kernel/start.c
@@ -0,0 +1,14 @@
+
+void main();
+
+
+// entry.S needs one stack per CPU.
+__attribute__ ((aligned (16))) char stack0[4096];
+
+// entry.S jumps here in machine mode on stack0.
+void
+start()
+{
+
+}
+
